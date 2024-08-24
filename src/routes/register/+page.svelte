062584e4f4f93beb63b442c2a4c07c0c96c3f68f
JoshuamURD <jmacmaster9@gmail.com>
2024-08-24 09:42:59 +0800

    Adding tailwind

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -27,62 +27,52 @@
 {#if error}
   <p class="error">{error}</p>
 {/if}
+<div class="flex flex-col min-h-screen justify-center gap-9">
+  <form
+    method="POST"
+    use:enhance={() => {
+      return async ({ result }) => {
+        if (result.type === "success") {
+          goto("/login");
+        } else if (result.type === "error") {
+          error = result.error.message;
+        }
+      };
+    }}
+    on:submit|preventDefault={validateForm}
+  >
+    <div>
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" bind:value={name} required />
+    </div>
+    <div>
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" bind:value={email} required />
+    </div>
+    <div>
+      <label for="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        bind:value={password}
+        required
+      />
+    </div>
+    <div>
+      <label for="passwordConfirm">Confirm Password:</label>
+      <input
+        type="password"
+        id="passwordConfirm"
+        bind:value={passwordConfirm}
+        required
+      />
+    </div>
+    <button type="submit">Register</button>
+    {#if form?.message}
+      <p class="error">{form.message}</p>
+    {/if}
+  </form>
 
-<form
-  method="POST"
-  use:enhance={() => {
-    return async ({ result }) => {
-      if (result.type === "success") {
-        goto("/login");
-      } else if (result.type === "error") {
-        error = result.error.message;
-      }
-    };
-  }}
-  on:submit|preventDefault={validateForm}
->
-  <div>
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" bind:value={name} required />
-  </div>
-  <div>
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" bind:value={email} required />
-  </div>
-  <div>
-    <label for="password">Password:</label>
-    <input
-      type="password"
-      id="password"
-      name="password"
-      bind:value={password}
-      required
-    />
-  </div>
-  <div>
-    <label for="passwordConfirm">Confirm Password:</label>
-    <input
-      type="password"
-      id="passwordConfirm"
-      bind:value={passwordConfirm}
-      required
-    />
-  </div>
-  <button type="submit">Register</button>
-  {#if form?.message}
-    <p class="error">{form.message}</p>
-  {/if}
-</form>
-
-<p>Already have an account? <a href="/login">Login</a></p>
-
-<style>
-  form {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-  }
-  .error {
-    color: red;
-  }
-</style>
+  <p>Already have an account? <a href="/login">Login</a></p>
+</div>
