361ac66e1d7dcd1c37e26db9ff9d119944ecdd35
JoshuamURD <jmacmaster9@gmail.com>
2024-08-31 00:14:33 +0800

    Adding navbar animation

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -2,18 +2,34 @@
   import { onMount } from "svelte";
   import { page } from "$app/stores";
   import { invalidateAll } from "$app/navigation";
+  import { fade, slide, fly } from "svelte/transition";
+  import { quintOut } from "svelte/easing";
 
   let user;
   let isDropdownOpen = false;
+  let isDropdownContentVisible = false;
 
   $: user = $page.data.user;
 
   function toggleDropdown() {
-    isDropdownOpen = !isDropdownOpen;
+    if (isDropdownOpen) {
+      isDropdownContentVisible = false;
+      setTimeout(() => {
+        isDropdownOpen = false;
+      }, 200); // Match this with the fade-out duration
+    } else {
+      isDropdownOpen = true;
+      setTimeout(() => {
+        isDropdownContentVisible = true;
+      }, 150); // Slightly less than the slide-down duration
+    }
   }
 
   function closeDropdown() {
-    isDropdownOpen = false;
+    isDropdownContentVisible = false;
+    setTimeout(() => {
+      isDropdownOpen = false;
+    }, 200); // Match this with the fade-out duration
   }
 
   async function handleLogout() {
@@ -38,10 +54,16 @@
   }
 </script>
 
-<div class="navbar bg-base-100 sticky top-0 z-50">
+<div
+  class="navbar bg-base-100 sticky top-0 z-50 shadow-md"
+  in:fade={{ duration: 300, delay: 150 }}
+>
   <div class="navbar-start">
     <div class="dropdown">
-      <button on:click={toggleDropdown} class="btn btn-ghost btn-circle">
+      <button
+        on:click={toggleDropdown}
+        class="btn btn-ghost btn-circle transition-transform duration-200 hover:scale-110"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-5 w-5"
@@ -58,41 +80,83 @@
         </svg>
       </button>
       {#if isDropdownOpen}
-        <ul
-          class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+        <div
+          class="dropdown-container mt-3 z-[1] shadow bg-base-100 rounded-box w-52 overflow-hidden"
+          transition:slide={{ duration: 200, easing: quintOut }}
         >
-          {#if user}
-            <li>
-              <button on:click={handleLogout}>Logout</button>
-            </li>
-            <li><a href="/add-book" on:click={closeDropdown}>Add book</a></li>
-            <li><a href="/groups" on:click={closeDropdown}>Groups</a></li>
-          {:else}
-            <li><a href="/register" on:click={closeDropdown}>Register</a></li>
-            <li><a href="/login" on:click={closeDropdown}>Login</a></li>
+          {#if isDropdownContentVisible}
+            <ul class="menu menu-sm p-2" transition:fade={{ duration: 200 }}>
+              {#if user}
+                <li>
+                  <button
+                    on:click={handleLogout}
+                    class="transition-colors duration-200 hover:bg-base-200"
+                    >Logout</button
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/add-book"
+                    on:click={closeDropdown}
+                    class="transition-colors duration-200 hover:bg-base-200"
+                    >Add book</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/groups"
+                    on:click={closeDropdown}
+                    class="transition-colors duration-200 hover:bg-base-200"
+                    >Groups</a
+                  >
+                </li>
+              {:else}
+                <li>
+                  <a
+                    href="/register"
+                    on:click={closeDropdown}
+                    class="transition-colors duration-200 hover:bg-base-200"
+                    >Register</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/login"
+                    on:click={closeDropdown}
+                    class="transition-colors duration-200 hover:bg-base-200"
+                    >Login</a
+                  >
+                </li>
+              {/if}
+            </ul>
           {/if}
-        </ul>
+        </div>
       {/if}
     </div>
   </div>
   <div class="navbar-center">
-    <a href="/" class="btn btn-ghost text-xl">Combined Learning List</a>
+    <a
+      href="/"
+      class="btn btn-ghost text-xl transition-all duration-300 hover:scale-105 hover:text-primary"
+      >Combined Learning List</a
+    >
   </div>
   <div class="navbar-end">
     <span class="text-sm">
       {#if user}
         <a
           href="/user/{user.id}"
-          class="btn btn-circle btn-ghost flex items-center justify-center"
+          class="btn btn-circle btn-ghost flex items-center justify-center transition-transform duration-200 hover:scale-110"
+          in:fly={{ y: -20, duration: 300, delay: 300, easing: quintOut }}
         >
           <div
-            class="w-10 h-10 rounded-full bg-base-300 flex items-center justify-center"
+            class="w-10 h-10 rounded-full bg-base-300 flex items-center justify-center overflow-hidden"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              class="w-6 h-6 text-base-content"
+              class="w-6 h-6 text-base-content transition-transform duration-300 hover:scale-125"
             >
               <path
                 fill-rule="evenodd"
@@ -103,8 +167,20 @@
           </div>
         </a>
       {:else}
-        Not logged in
+        <span in:fade={{ duration: 300, delay: 300 }}>Not logged in</span>
       {/if}
     </span>
   </div>
 </div>
+
+<style>
+  .dropdown {
+    position: relative;
+  }
+
+  .dropdown-container {
+    position: absolute;
+    top: 100%;
+    left: 0;
+  }
+</style>
